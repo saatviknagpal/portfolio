508b9880bbdc14f8b4b1a0c966423bd21e2ba4ca
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-02-19 17:52:46 +0530

    Added Projects in Portfolio Section

--- src/Util/data.js
+++ src/Util/data.js
@@ -82,45 +82,40 @@ export const educationData = [
     icon: "school",
   },
 ];
-  export const webApps = [
-    {
-      id: 1,
-      title: "Jumping Bunny Game",
-      img:
-        "https://user-images.githubusercontent.com/58262578/118312077-70b4d180-b50e-11eb-8cc7-cef8d1c1671a.png",
-      link: "https://github.com",
-    },
-  ];
+export const frontend = [
+  {
+    id: 1,
+    title: "Airbnb Clone",
+    img: "https://user-images.githubusercontent.com/61618767/154679179-21aa77dc-d296-4082-b0a1-b88d0c7b2575.png",
+    link: "https://github.com/saatviknagpal/airbnb-clone",
+    icon: "devicon-github-original",
+    live: "https://airbnb-nextjsclone.vercel.app/"
+  },
+  {
+    id: 2,
+    title: "DSC ADGITM Website",
+    img: "https://user-images.githubusercontent.com/61618767/154679766-9dc562be-2f3d-48d7-b5b2-7d43504991dd.png",
+    link: "https://github.com/Adgitmdsc/dsc-adgitm-website",
+    icon: "devicon-github-original",
+    live: "https://dsc-adgitm.web.app/"
+  },
+  {
+    id: 3,
+    title: "Jumping Bunny Game",
+    img: "https://user-images.githubusercontent.com/58262578/118312077-70b4d180-b50e-11eb-8cc7-cef8d1c1671a.png",
+    link: "https://github.com/saatviknagpal/Jumping-Bunny-Game",
+    icon: "devicon-github-original",
+    live: "https://saatviknagpal.github.io/Jumping-Bunny-Game/"
+  },
+];
+
+export const fullStack = [
+  {
+    id: 1,
+    title: "Mind Matters (Hackathon)",
+    img: "https://user-images.githubusercontent.com/61618767/153759171-d404b2fd-f147-4978-b40a-2838775d75e6.png",
+    link: "https://github.com/saatviknagpal/Mind-Matters",
+    icon: "devicon-github-original",
+  },
   
-  export const webDesign = [
-    {
-      id: 1,
-      title: "Web Social Media App",
-      img: "https://cdn.dribbble.com/users/5031392/screenshots/15467520/media/c36b3b15b25b1e190d081abdbbf947cf.png?compress=1&resize=1200x900",
-    },
-    {
-      id: 2,
-      title: "Web Rampa UI Design",
-      img: "https://cdn.dribbble.com/users/3967258/screenshots/15463803/media/4fddb9a2caf3b3bd634060f706a91e73.png?compress=1&resize=1200x900",
-    },
-    {
-      id: 3,
-      title: "Web E-commerce Design",
-      img: "https://cdn.dribbble.com/users/3637068/screenshots/15467010/media/69a3279182b75365013fe285733d1122.png?compress=1&resize=1200x900",
-    },
-    {
-      id: 4,
-      title: "Web Relax App",
-      img: "https://cdn.dribbble.com/users/2264844/screenshots/15463998/media/d85e92a332c41094f68e1f716884f7ce.jpg?compress=1&resize=1200x900",
-    },
-    {
-      id: 5,
-      title: "Web Design",
-      img: "https://cdn.dribbble.com/users/387658/screenshots/15458608/media/e92ec1dd33dd1170ae3dc9c5272f2693.jpg?compress=1&resize=1200x900",
-    },
-    {
-      id: 6,
-      title: "Web Banking App",
-      img: "https://cdn.dribbble.com/users/6384483/screenshots/15468426/media/ce9479fa1f8dba3a4a49840d76e55e31.png?compress=1&resize=1200x900",
-    },
-  ];
+];
