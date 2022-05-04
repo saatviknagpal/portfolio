58dcefdf7ecff17e723b7aac16856b99aec9f91c
Saatvik Nagpal <Saatvikngpl21@gmail.com>
2022-05-04 18:29:09 +0530

    Update About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -17,7 +17,7 @@ export default function About() {
           Apart from my technical side, I love to play video games, listen to
           music and enjoy going for walks.
         </p>
-        <a href="https://drive.google.com/file/d/1HYssmg2tbtqGc1wivssKZ8Sl9prjERcF/view">View Resume</a>
+        <a href="https://drive.google.com/file/d/1D0XObvLs0vJ2sJpXh4IXWL4aSXOuaead/view?usp=sharing">View Resume</a>
       </div>
       <div className="skills">
         <Skills />
